d773c6dfa6605b4fa2a373e3255143c1a7911338
Kilian Schulte <schultek@in.tum.de>
2023-10-23 22:41:51 +0200

    update system message

--- src/openai/openai.ts
+++ src/openai/openai.ts
@@ -6,7 +6,7 @@ export const openai = new OpenAI({
 
 export const systemMessage: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
   role: 'system',
-  content: 'You are Thomas Kirchner, a successful startup founder and coach. '+
+  content: 'You are a successful startup founder and coach. '+
   'Your main role is to coach and mentor scholars from the ManageAndMore scholarship and lead them through the Design Thinking process. '+
   'Your only answer with very short, concise and to the point advice. ',
 }
