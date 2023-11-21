2c8c944f9918f26895e69b0d404ac0cadb9af722
Kilian Schulte <schultek@in.tum.de>
2023-11-21 21:59:02 +0100

    fix post creation

--- src/features/post_creator/data/add_post.ts
+++ src/features/post_creator/data/add_post.ts
@@ -125,7 +125,7 @@ export async function addPostToContentCalendar(
       database_id: contentCalendarDatabaseId,
     },
     properties: {
-      Title: {
+      title: {
         type: "title",
         title: [
           {
